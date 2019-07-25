168214e3761413062d8fc19bf1cd7c25c53b3ace
Santiago Bandiera <sagaban@hotmail.com>
2019-07-25 19:46:08 -0300

    Defer the installation

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
-// The Navigator interface represents the state and the identity of the user agent. It allows
-// scripts to query it and to register themselves to carry on some activities.
+let deferredPrompt;
+/**
+ * The Navigator interface represents the state and the identity of the user
+ * agent. It allows scripts to query it and to register themselves to carry on
+ * some activities.
+ */
 if ("serviceWorker" in navigator) {
   // `{ scope: "/help/" }` could be passed as second argument to define the SW scope
   // It only works with restrictive (more specific) scopes
@@ -7,3 +11,22 @@ if ("serviceWorker" in navigator) {
     console.log("Service worker registered!");
   });
 }
+
+window.addEventListener("beforeinstallprompt", event => {
+  console.log("beforeinstallprompt triggered...");
+  // Prevent Chrome <= 67 from automatically showing the prompt
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+  return false;
+});
+
+/*
+In case we want/need to unregister a SW
+
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+  for (let registration of registrations) {
+    registration.unregister();
+  }
+});
+*/
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,33 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+  // Check if the PWA install event is stashed.
+  if (deferredPrompt) {
+    // Show the modal add to home screen dialog
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choice => {
+      if (choice.outcome === "accepted") {
+        // or !== 'dismissed'
+        console.log("User accepted the installation");
+      } else {
+        console.log("User dismissed the installation");
+      }
+      // Clear the saved prompt since it can't be used again
+      deferredPrompt = null;
+    });
+  }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
