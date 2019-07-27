90afce7541f7e9da7273bdea442de7893e9540b4
Santiago Bandiera <sagaban@hotmail.com>
2019-07-26 22:51:30 -0300

    Cache the most of our app shell

--- public/sw.js
+++ public/sw.js
@@ -14,10 +14,26 @@ self.addEventListener("install", event => {
     caches.open("static").then(cache => {
       console.log("[Service Worker] Precaching App Shell");
       // With add, we execute the request to fetch the resource
-      cache.add("/");
       // We cache REQUESTs, so, we need to cache `/` to store the `index.html`
-      cache.add("/index.html");
-      cache.add("/src/js/app.js");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        // If polyfills are needed, SW is not supported. No need to cache.
+        // Should conditionally loaded in our work flow
+        // Added for performance issues
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        // Maybe the icons, it depends
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]);
     })
   );
 });
